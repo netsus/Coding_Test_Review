d231d8f9dcdc9a25b2208c3dfcedda8eee74b959
netsus <cwjcsk@naver.com>
2023-05-25 13:45:42 +0900

    풀이 완료

---
+++ 05_BruteForce/백준_1759_암호만들기.py
@@ -0,0 +1,32 @@
+"""
+날짜 <2023/05/18 (목)>
+- 문제 링크: 
+- 문제 요약
+
+- 상상 코딩
+
+- 체감 난이도 (1:쉬움 ~ 5:매우 어려움)
+
+- 문제 분석
+
+    - 알고리즘 유형 분석
+
+    - 시간복잡도 분석
+
+    - 리팩토링 방향성
+    
+"""
+## 파일로 예제 입력 읽기
+import sys
+sys.stdin = open("input.txt",'r')
+##
+import sys
+input = sys.stdin.readline
+from itertools import combinations
+L,C = map(int,input().split())
+words = sorted(input().split())
+for word in combinations(words,L):
+    m_cnt = sum(1 for w in word if w in 'aeiou')
+    if m_cnt >= 1 and len(word) - m_cnt >= 2:
+        print(''.join(word))
+
